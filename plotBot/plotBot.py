90f08441a4d24fa9534f66ce42ea680895ced1ac
Dan Seminara <dseminara@50onred.com>
2014-07-31 11:53:05 -0400

    Nice json

--- plotBot/plotBot.py
+++ plotBot/plotBot.py
@@ -4,4 +4,4 @@ from redis import StrictRedis
 db = StrictRedis(host='localhost', port=6379)
 jokes = [dict(json.loads(db.get(k))['log_data'].items() + [('jokeId', k)]) for k in db.keys('jokes:*')]
 with open('jokedata.json', 'w') as f:
-    json.dump(jokes, f)
+    json.dump(jokes, f, indent=2)
