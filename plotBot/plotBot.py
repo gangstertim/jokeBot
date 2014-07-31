63bf23a394c8564391756c9139944e8858627c03
Dan Seminara <dseminara@50onred.com>
2014-07-31 11:50:43 -0400

    Fix typo

--- plotBot/plotBot.py
+++ plotBot/plotBot.py
@@ -2,6 +2,6 @@ import json
 from redis import StrictRedis
 
 db = StrictRedis(host='localhost', port=6379)
-jokes = [dict(json.loads(db.get(k))['logdata'].items() + [('jokeId', k)]) for k in db.keys('jokes:*')]
+jokes = [dict(json.loads(db.get(k))['log_data'].items() + [('jokeId', k)]) for k in db.keys('jokes:*')]
 with open('jokedata.json', 'w') as f:
     json.dump(jokes, f)
